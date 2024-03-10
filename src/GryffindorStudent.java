9a37ae80dad443d1cc4fa380d71a648af7a8e4ea
marauder569 <lex_hunter_94@mail.ru>
2024-03-10 15:58:28 +0300

    homework oop

---
+++ src/GryffindorStudent.java
@@ -0,0 +1,29 @@
+class GryffindorStudent extends HogwartsStudents {
+    private final int nobility;
+    private final int honor;
+    private final int bravery;
+
+    public GryffindorStudent(String name, int magicPower, int transgressionDistance, int nobility, int honor, int bravery) {
+        super(name, magicPower, transgressionDistance);
+        this.nobility = nobility;
+        this.honor = honor;
+        this.bravery = bravery;
+    }
+
+    public int getNobility() {
+        return nobility;
+    }
+
+    public int getHonor() {
+        return honor;
+    }
+
+    public int getBravery() {
+        return bravery;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
