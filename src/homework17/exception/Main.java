d6e75e9640603f283c5c0143741cf068c0c25166
marauder569 <lex_hunter_94@mail.ru>
2024-04-26 19:15:29 +0300

    homework exception

---
+++ src/homework17/exception/Main.java
@@ -0,0 +1,8 @@
+package homework17.exception;
+
+public class Main {
+    public static void main(String[] args) {
+        // Пример использования методов валидации
+        User.validate("java_skypro", "D_1hWiKjP_9", "D_1hWiKjjP_9");
+    }
+}
